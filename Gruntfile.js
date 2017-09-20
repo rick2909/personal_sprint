23d42afb0df0427b54f829518299eab1c2c1ef21
Rick van Nieuwland <rick29998@gmail.com>
2017-09-20 13:53:38 +0200

    made it so grunt is in separated files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,41 +1,15 @@
 'use strict';
 
 module.exports = function(grunt) {
-  // Add configuration, tasks and plugins here
-  	grunt.initConfig({
-		// imports the config data from the package.json
-		pkg: grunt.file.readJSON('package.json'),
-	    watch: {
-		    files: 'assets/sass/**/*.scss',
-		    tasks: ['sass', 'cssmin'],
-		    options: {
-		      livereload: true,
-		    },
-		},
-		sass: {
-	        dist: {
-	            files: {
-	                'assets/css/main.css': 'assets/sass/style.scss'
-	            },
-	        },
-		},
-		cssmin: {
-			target: {
-				files: [{
-					expand: true,
-					cwd: 'assets/css',
-					src: ['*.css', '!*.min.css'],
-					dest: 'build/css',
-					ext: '.min.css'
-				}]
-			}
-		}
-	});
 	// Load the plugin that provides the tasks.
-	//grunt.loadNpmTasks('grunt-contrib-uglify');
+    //grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    
+    //load the various tasks configuration files
+    var configs = require('load-grunt-configs') (grunt);
+    grunt.initConfig(configs);
 
 	// Default task(s).
 	grunt.registerTask('default', ['watch']);
