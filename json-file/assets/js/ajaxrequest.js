e06ee23b156ebf06c567597f78fbaace95fa8d1b
Rick van Nieuwland <rick29998@gmail.com>
2017-11-14 14:31:54 +0100

    add json file and read it out

--- json-file/assets/js/ajaxrequest.js
+++ json-file/assets/js/ajaxrequest.js
@@ -1,16 +1,7 @@
-function AJAX_JSON_Req( url )
+function ajaxRequest( url )
 {
-    var AJAX_req = new XMLHttpRequest();
-    AJAX_req.open( "GET", url, true );
-    AJAX_req.setRequestHeader("Content-type", "application/json");
- 
-    AJAX_req.onreadystatechange = function()
-    {
-        if( AJAX_req.readyState == 4 && AJAX_req.status == 200 )
-        {
-            var response = JSON.parse( AJAX_req.responseText );
-            document.write( response.name );
-        }
-    }
-    AJAX_req.send();
+	var AJAX_req = new XMLHttpRequest();
+	AJAX_req.open( 'GET', url, false );
+	AJAX_req.send( null );
+	return AJAX_req.responseText;
 }
